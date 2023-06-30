9f2363477880f9a85a12f4f909ffa5df40cd7e0b
Glenn Latomme <glenn.latomme@gmail.com>
2023-06-30 15:44:16 +0200

    ci: this should be the folder

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -4,12 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   template: `
-    <div>
-      <a href="https://analogjs.org/" target="_blank">
-        <img alt="Analog Logo" class="logo analog" src="/analog.svg" />
-      </a>
-    </div>
-
     <h2>Analog</h2>
 
     <h3>The fullstack meta-framework for Angular!</h3>
@@ -25,15 +19,6 @@ import { Component } from '@angular/core';
   `,
   styles: [
     `
-      .logo {
-        will-change: filter;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.angular:hover {
-        filter: drop-shadow(0 0 2em #42b883aa);
-      }
       .read-the-docs {
         color: #888;
       }
