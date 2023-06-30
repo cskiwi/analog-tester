8f33a8cc9b7484fbe453e282c3bf203e0772ce83
Glenn Latomme <glenn.latomme@gmail.com>
2023-06-30 14:46:58 +0200

    ci: switch to vercel?

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [
+    analog({
+      nitro: {
+        preset: 'vercel',
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
